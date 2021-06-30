dcf31d0ec650831dede0391457aacb4da806010c
Jim Browning <jamalebrowning@gmail.com>
2021-06-30 14:32:09 -0500

    added test for combinations

---
+++ test_game.py
@@ -0,0 +1,54 @@
+import unittest
+from unittest import result
+import main
+
+
+class TestPlay(unittest.TestCase):
+    '''
+    Rock = r , Paper = p , Scissors = s
+    Test for 
+    r/r , r/p , r/s
+    s/s , s/p , s/s
+    p/p , p/s, p/r
+    '''  
+
+
+    def test_play_tie_rock(self):
+      result = main.is_win('r', 'r')
+      self.assertEqual(result, False)
+
+    def test_play_tie_scissors(self):
+      result = main.is_win('s', 's')
+      self.assertEqual(result, False)
+
+    def test_play_tie_paper(self):
+      result = main.is_win('p', 'p')
+      self.assertEqual(result, False)   
+
+    def test_play_win_rock_scissors(self):
+      result = main.is_win('r', 's')
+      self.assertEqual(result, True)
+
+    def test_play_win_rock_paper(self):
+      result = main.is_win('r', 'p')
+      self.assertEqual(result, False)
+
+    def test_play_win_paper_scissors(self):
+      result = main.is_win('p', 's')
+      self.assertEqual(result, False) 
+
+    def test_play_win_paper_rock(self):
+      result = main.is_win('p', 'r')
+      self.assertEqual(result, True)  
+
+    def test_play_win_scissors_paper(self):
+      result = main.is_win('s', 'p')
+      self.assertEqual(result, True)
+
+    def test_play_win_scissors_rock(self):
+      result = main.is_win('s', 'r')
+      self.assertEqual(result, False)   
+
+
+if __name__ == '__main__':
+  unittest.main()
